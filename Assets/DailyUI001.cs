b305c7ce42a172291cf37eccdbe208a52670c2ec
emontecalvo <emilymontecalvo@gmail.com>
2017-05-08 09:35:58 -0700

    barebones day 001

---
+++ Assets/DailyUI001.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DailyUI001 : MonoBehaviour 
+{
+	public InputField NameIF;
+	public InputField EmailIF;
+	public Button SubmitButton;
+	// Use this for initialization
+	void Start () {
+		SubmitButton.onClick.AddListener (onClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onClick() {
+		Debug.LogFormat ("I am clicked {0}{1}", NameIF.text, EmailIF.text);
+//		NameIF.text = null;
+//		EmailIF.text = null;
+	}
+}
---
+++ Assets/DailyUIManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DailyUIManager : MonoBehaviour {
+	public enum EScreenType{GalaxyS6, IPhone7};
+	public EScreenType ScreenType;
+	public RectTransform Envelope;
+	// Use this for initialization
+
+	void OnValidate () {
+		if (ScreenType == EScreenType.GalaxyS6) {
+			Envelope.sizeDelta = new Vector2(1440, 2560);
+		} 
+		if (ScreenType == EScreenType.IPhone7) {
+			Envelope.sizeDelta = new Vector2(750, 1334);
+		} 
+	}
+	void Start () {
+	
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
