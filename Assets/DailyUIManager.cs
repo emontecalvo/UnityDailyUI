d41322ac063b97921663a8f5f9fc263aca13ab07
emontecalvo <emilymontecalvo@gmail.com>
2017-05-09 09:32:57 -0700

    upgraded day 001 from a layout group to setting anchors, added background img

--- Assets/DailyUIManager.cs
+++ Assets/DailyUIManager.cs
@@ -3,7 +3,11 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class DailyUIManager : MonoBehaviour {
-	public enum EScreenType{GalaxyS6, IPhone7};
+	public enum EScreenType{
+		GalaxyS6, 
+		IPhone7,
+		Dev
+	};
 	public EScreenType ScreenType;
 	public RectTransform Envelope;
 	// Use this for initialization
@@ -14,6 +18,9 @@ public class DailyUIManager : MonoBehaviour {
 		} 
 		if (ScreenType == EScreenType.IPhone7) {
 			Envelope.sizeDelta = new Vector2(750, 1334);
+		} 
+		if (ScreenType == EScreenType.Dev) {
+			Envelope.sizeDelta = new Vector2(720, 1080);
 		} 
 	}
 	void Start () {
