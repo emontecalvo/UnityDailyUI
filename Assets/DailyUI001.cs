3feed738f87931cadc015f8bb1e5a958c54de06b
emontecalvo <emilymontecalvo@gmail.com>
2017-05-09 10:09:51 -0700

    imported DOTween, added animation for signup box on start

--- Assets/DailyUI001.cs
+++ Assets/DailyUI001.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
 public class DailyUI001 : MonoBehaviour 
 {
+	public RectTransform FrameRT;
 	public InputField NameIF;
 	public InputField EmailIF;
 	public Button SubmitButton;
+	public Image BackgroundImage;
+
 	// Use this for initialization
 	void Start () {
 		SubmitButton.onClick.AddListener (onClick);
+//		ShortcutExtensions46.DOAnchorPos;
+//		MyRT.DOAnchorPos (new Vector2 (-1, 0), 1, true);
+
+//		FrameRT.offsetMin = new Vector2 (-1000, 0);
+//		FrameRT.offsetMax = new Vector2 (-1000, 0);
+
+//		DOTween.To (() => FrameRT.offsetMin, x => FrameRT.offsetMin = x, new Vector2 (0, 0), 1f);
+//		DOTween.To (() => FrameRT.offsetMax, x => FrameRT.offsetMax = x, new Vector2 (0, 0), 1f);
+		float time = 3f;
+
+		Vector3 dest = FrameRT.localPosition;
+		Vector3 start = dest + new Vector3 (-1000, 0, 0);
+
+		FrameRT.localPosition = start;
+		DOTween.To (() => FrameRT.localPosition, x => FrameRT.localPosition = x, dest, time);
+
+		Color DestColor = BackgroundImage.color;
+		Color StartColor = DestColor;
+		StartColor.a = 0;
+		BackgroundImage.color = StartColor;
+		DOTween.To (() => BackgroundImage.color, x => BackgroundImage.color = x, DestColor, time);
 	}
 	
 	// Update is called once per frame
@@ -21,5 +46,12 @@ public class DailyUI001 : MonoBehaviour
 		Debug.LogFormat ("I am clicked {0}{1}", NameIF.text, EmailIF.text);
 //		NameIF.text = null;
 //		EmailIF.text = null;
+		float time = 3f;
+		
+		Vector3 start = FrameRT.localPosition;
+		Vector3 dest = start + new Vector3 (-1000, 0, 0);
+		
+		FrameRT.localPosition = start;
+		DOTween.To (() => FrameRT.localPosition, x => FrameRT.localPosition = x, dest, time);
 	}
 }
