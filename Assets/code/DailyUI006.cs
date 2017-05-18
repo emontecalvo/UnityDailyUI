15d03f193e906a1bba8c902254eeefe43bfaec53
emontecalvo <emilymontecalvo@gmail.com>
2017-05-18 15:51:35 -0700

    Day005 - added follow/unfollow functionality

--- Assets/code/DailyUI006.cs
+++ Assets/code/DailyUI006.cs
@@ -5,6 +5,33 @@ using DG.Tweening;
 
 public class DailyUI006 : DailyUIBase
 {
+	public Text NumOfFollowers;
+	public Text FollowOrNot;
+	public Button Follow;
+	public float ActualNumber;
 
+	bool IsFollowing = false;
+
+	void Start(){
+		Follow.onClick.AddListener (FollowCheck);
+	}
+
+	void FollowCheck(){
+		if (IsFollowing == false) {
+			ActualNumber += 1;
+			FollowOrNot.text = "unfollow";
+			IsFollowing = true;
+			UpdateDisplay ();
+		} else {
+			ActualNumber -= 1;
+			FollowOrNot.text = "follow";
+			IsFollowing = false;
+			UpdateDisplay ();
+		}
+	}
+
+	void UpdateDisplay(){
+		NumOfFollowers.text = ActualNumber.ToString();
+	}
 }
 
