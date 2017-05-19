e4f15f7ab68a7f11cb0f345d207ef8655455e7f7
emontecalvo <emilymontecalvo@gmail.com>
2017-05-19 13:30:00 -0700

    Day007 - starting slider code

--- Assets/code/DailyUI007.cs
+++ Assets/code/DailyUI007.cs
@@ -5,6 +5,13 @@ using DG.Tweening;
 
 public class DailyUI007 : DailyUIBase
 {
+	public Slider Volume;
+	public Slider Days;
+	public Slider Morale;
+	public Slider RandomEvents;
 
+	void Start() {
+
+	}
 }
 
