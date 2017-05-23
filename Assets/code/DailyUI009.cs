cace92bf7e5af6da8fa5a861e149ae44e18ed852
emontecalvo <emilymontecalvo@gmail.com>
2017-05-23 14:58:12 -0700

    +Day009 - music player - basic layout added

---
+++ Assets/code/DailyUI009.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class DailyUI009 : DailyUIBase
+{
+	public Button PlayOrPauseBtn;
+	public Button PreviousSongBtn;
+	public Button NextSongBtn;
+	public Button HeartBtn;
+	public Text MusicPlayedTxt;
+	public Text MusicLeftTxt;
+	public Slider MusicBarSlider;
+	public Sprite PlayImage;
+	public Sprite PauseImage;
+	public Sprite HeartEmpty;
+	public Sprite HeartFull;
+
+	bool PlayMusic = false;
+	bool HeartClickedBool = false;
+
+	void Start() {
+		PlayOrPauseBtn.onClick.AddListener (PlayPauseClicked);
+		HeartBtn.onClick.AddListener (HeartClicked);
+	
+	}
+
+	void PlayPauseClicked(){
+		if (!PlayMusic) {
+			PlayMusic = true;
+			PlayOrPauseBtn.image.sprite = PlayImage;
+
+		} else {
+			PlayMusic = false;
+			PlayOrPauseBtn.image.sprite = PauseImage;
+		}
+	}
+
+	void HeartClicked(){
+		if (!HeartClickedBool) {
+			HeartBtn.image.sprite = HeartFull;
+			HeartClickedBool = true;
+		} else {
+			HeartBtn.image.sprite = HeartEmpty;
+			HeartClickedBool = false;
+		}
+	}
+
+
+}
