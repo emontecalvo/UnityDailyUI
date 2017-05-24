f4043c221f69743b6e4553930d933df23f584ddb
emontecalvo <emilymontecalvo@gmail.com>
2017-05-24 14:16:18 -0700

    +Day010 - social share - added basic layout and art

---
+++ Assets/code/DailyUI010.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class DailyUI010 : DailyUIBase
+{
+	public Button ShareIconBtn;
+
+	void Start(){
+		ShareIconBtn.onClick.AddListener (OnShareClicked);
+	}
+
+	void OnShareClicked(){
+		Debug.Log ("I am clicked");
+	}
+}
