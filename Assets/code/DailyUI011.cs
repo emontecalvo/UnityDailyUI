aacc5f1ab8a559b0d4c7610dece1de5c9ca6aa7a
emontecalvo <emilymontecalvo@gmail.com>
2017-05-25 13:08:40 -0700

    +011 - success-failure-flash - basic layout added

---
+++ Assets/code/DailyUI011.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class DailyUI011 : DailyUIBase
+{
+
+
+	void Start(){
+
+	}
+
+
+}
