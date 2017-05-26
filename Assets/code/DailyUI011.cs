e6afae641b41cfe42759c5aa3e49064944f66d1e
emontecalvo <emilymontecalvo@gmail.com>
2017-05-26 08:50:15 -0700

    Day011 - added fade in / fade out Dotween

--- Assets/code/DailyUI011.cs
+++ Assets/code/DailyUI011.cs
@@ -6,11 +6,21 @@ using DG.Tweening;
 
 public class DailyUI011 : DailyUIBase
 {
-
+	public CanvasGroup SuccessCG;
+	public CanvasGroup DratCG;
+	const float TransitionTime = 5f;
 
 	void Start(){
-
+		DratFadeIn ();
 	}
 
+	void DratFadeIn(){
+		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 1, TransitionTime).OnComplete(SuccessFadeIn);
+	}
 
+	void SuccessFadeIn(){
+		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 1, TransitionTime).OnComplete(DratFadeIn);
+	}
 }
