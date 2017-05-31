93147ca4c20d6101c84208500429f93439aef866
emontecalvo <emilymontecalvo@gmail.com>
2017-05-31 09:25:50 -0700

    +Day013 - basic layout started

---
+++ Assets/code/DailyUI013.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class DailyUI013 : DailyUIBase
+{
+
+
+	void Start(){
+
+	}
+
+
+}
