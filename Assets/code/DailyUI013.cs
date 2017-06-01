019ba7e8215cd19c670cea0b1dcc907b37ecadb6
emontecalvo <emilymontecalvo@gmail.com>
2017-05-31 20:20:53 -0700

    Day013 - updated button functionality away from brute force

--- Assets/code/DailyUI013.cs
+++ Assets/code/DailyUI013.cs
@@ -10,54 +10,14 @@ public class DailyUI013 : DailyUIBase
 	public Text SendMsgBtnText;
 
 	public Text SentTextDisplay;
-
 	public Text TypingTextDisplay;
 
-	public Button OneBtn;
-	public Button TwoBtn;
-	public Button ThreeBtn;
-	public Button FourBtn;
-	public Button FiveBtn;
-	public Button SixBtn;
-	public Button SevenBtn;
-	public Button EightBtn;
-	public Button NineBtn;
-	public Button ZeroBtn;
-	public Button ABtn;
-	public Button BBtn;
-	public Button CBtn;
-	public Button DBtn;
-	public Button EBtn;
-	public Button FBtn;
-	public Button GBtn;
-	public Button HBtn;
-	public Button IBtn;
-	public Button JBtn;
-	public Button KBtn;
-	public Button LBtn;
-	public Button MBtn;
-	public Button NBtn;
-	public Button OBtn;
-	public Button PBtn;
-	public Button QBtn;
-	public Button RBtn;
-	public Button SBtn;
-	public Button TBtn;
-	public Button UBtn;
-	public Button VBtn;
-	public Button WBtn;
-	public Button XBtn;
-	public Button YBtn;
-	public Button ZBtn;
+	public List<Button> TextButtons = new List<Button> ();
+
+
 	public Button PaperclipBtn;
 	public Button SmileyBtn;
-	public Button PeriodBtn;
-	public Button QuestionBtn;
-	public Button ExclamationBtn;
-	public Button MoneyBtn;
-	public Button AndBtn;
-	public Button AtBtn;
-	public Button SpaceBtn;
+
 
 	bool MessageNotSend = true;
 	const float TransitionTime = 1f;
@@ -66,17 +26,18 @@ public class DailyUI013 : DailyUIBase
 
 	void Start(){
 		SendMsgBtn.onClick.AddListener (MsgOrSendClicked);
-		HBtn.onClick.AddListener (HBtnClicked);
-		IBtn.onClick.AddListener (IBtnClicked);
+
+		for (int i = 0; i < TextButtons.Count; i++) {
+			int j = i;
+			TextButtons [i].onClick.AddListener(() => { TextButtonClicked(j); });
+		}
+
 	}
 
-	void HBtnClicked() {
-		TypingTextDisplay.text += "h";
-		SentTextDisplay.text = TypingTextDisplay.text;
-	}
-
-	void IBtnClicked() {
-		TypingTextDisplay.text += "i";
+	void TextButtonClicked(int k) {
+		Button btn = TextButtons [k];
+		Text letter = btn.GetComponentInChildren<Text> ();
+		TypingTextDisplay.text += letter.text;
 		SentTextDisplay.text = TypingTextDisplay.text;
 	}
 
