b71b602bda70b8eaa771bf117c2dc610eeb08ebe
emontecalvo <emilymontecalvo@gmail.com>
2017-06-05 12:41:08 -0700

    Day014 - basic transitions added

--- Assets/code/DailyUI014.cs
+++ Assets/code/DailyUI014.cs
@@ -6,21 +6,65 @@ using DG.Tweening;
 
 public class DailyUI014 : DailyUIBase
 {
-//	public CanvasGroup SuccessCG;
-//	public CanvasGroup DratCG;
-//	const float TransitionTime = 5f;
+	public CanvasGroup FullCloudCG;
+	public CanvasGroup ThreeQuarterCloudCG;
+	public CanvasGroup HalfCloudCG;
+	public CanvasGroup QuarterCloudCG;
+	public CanvasGroup EmptyCloudCG;
+
+	public CanvasGroup UmbrellaOneCG;
+	public CanvasGroup UmbrellaTwoCG;
+	public CanvasGroup UmbrellaThreeCG;
+	public CanvasGroup UmbrellaFourCG;
+	public CanvasGroup UmbrellaFiveCG;
+
+	public CanvasGroup DarkBackgroundCG;
+	public CanvasGroup LightBackgroundCG;
+
+	const float TransitionTime = 2f;
+	const float UmbrellaTransitionTime = 1.7f;
 //
-//	void Start(){
-//		DratFadeIn ();
-//	}
-//
-//	void DratFadeIn(){
-//		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 0, TransitionTime);
-//		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 1, TransitionTime).OnComplete(SuccessFadeIn);
-//	}
-//
-//	void SuccessFadeIn(){
-//		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 0, TransitionTime);
-//		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 1, TransitionTime).OnComplete(DratFadeIn);
-//	}
+	void Start(){
+		ThreeQuarterFadeIn ();
+	}
+
+	void ThreeQuarterFadeIn (){
+		DOTween.To (() => FullCloudCG.alpha, x => FullCloudCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => UmbrellaOneCG.alpha, x => UmbrellaOneCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => UmbrellaTwoCG.alpha, x => UmbrellaTwoCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => ThreeQuarterCloudCG.alpha, x => ThreeQuarterCloudCG.alpha = x, 1, TransitionTime).OnComplete(HalfFadeIn);
+	}
+
+	void HalfFadeIn(){
+		DOTween.To (() => ThreeQuarterCloudCG.alpha, x => ThreeQuarterCloudCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => UmbrellaTwoCG.alpha, x => UmbrellaTwoCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => UmbrellaThreeCG.alpha, x => UmbrellaThreeCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => HalfCloudCG.alpha, x => HalfCloudCG.alpha = x, 1, TransitionTime).OnComplete(QuarterFadeIn);
+	}
+
+	void QuarterFadeIn(){
+		DOTween.To (() => HalfCloudCG.alpha, x => HalfCloudCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => UmbrellaThreeCG.alpha, x => UmbrellaThreeCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => UmbrellaFourCG.alpha, x => UmbrellaFourCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => QuarterCloudCG.alpha, x => QuarterCloudCG.alpha = x, 1, TransitionTime).OnComplete(EmptyFadeIn);
+	}
+
+	void EmptyFadeIn(){
+		DOTween.To (() => QuarterCloudCG.alpha, x => QuarterCloudCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => UmbrellaFourCG.alpha, x => UmbrellaFourCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => UmbrellaFiveCG.alpha, x => UmbrellaFiveCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => DarkBackgroundCG.alpha, x => DarkBackgroundCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => LightBackgroundCG.alpha, x => LightBackgroundCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => EmptyCloudCG.alpha, x => EmptyCloudCG.alpha = x, 1, TransitionTime).OnComplete(FullFadeIn);
+	}
+
+	void FullFadeIn(){
+		DOTween.To (() => EmptyCloudCG.alpha, x => EmptyCloudCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => UmbrellaFiveCG.alpha, x => UmbrellaFiveCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => UmbrellaOneCG.alpha, x => UmbrellaOneCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => LightBackgroundCG.alpha, x => LightBackgroundCG.alpha = x, 0, UmbrellaTransitionTime);
+		DOTween.To (() => DarkBackgroundCG.alpha, x => DarkBackgroundCG.alpha = x, 1, UmbrellaTransitionTime);
+		DOTween.To (() => FullCloudCG.alpha, x => FullCloudCG.alpha = x, 1, TransitionTime).OnComplete(ThreeQuarterFadeIn);
+	}
+
 }
