0c7e1f59ea716b68a2ceac06fdf4f27dad428b56
emontecalvo <emilymontecalvo@gmail.com>
2017-06-05 11:53:45 -0700

    +Day014 - basic layout

---
+++ Assets/code/DailyUI014.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class DailyUI014 : DailyUIBase
+{
+//	public CanvasGroup SuccessCG;
+//	public CanvasGroup DratCG;
+//	const float TransitionTime = 5f;
+//
+//	void Start(){
+//		DratFadeIn ();
+//	}
+//
+//	void DratFadeIn(){
+//		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 0, TransitionTime);
+//		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 1, TransitionTime).OnComplete(SuccessFadeIn);
+//	}
+//
+//	void SuccessFadeIn(){
+//		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 0, TransitionTime);
+//		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 1, TransitionTime).OnComplete(DratFadeIn);
+//	}
+}
