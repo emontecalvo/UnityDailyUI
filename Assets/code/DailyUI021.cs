0d0f791294186fd928d03c08603869b775349bec
emontecalvo <emilymontecalvo@gmail.com>
2017-06-12 13:32:51 -0700

    Day021 - basic layout/transitions, need to update slider visual

--- Assets/code/DailyUI021.cs
+++ Assets/code/DailyUI021.cs
@@ -6,21 +6,22 @@ using DG.Tweening;
 
 public class DailyUI021 : DailyUIBase
 {
-//	public CanvasGroup SuccessCG;
-//	public CanvasGroup DratCG;
-//	const float TransitionTime = 5f;
-//
-//	void Start(){
-//		DratFadeIn ();
-//	}
-//
-//	void DratFadeIn(){
-//		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 0, TransitionTime);
-//		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 1, TransitionTime).OnComplete(SuccessFadeIn);
-//	}
-//
-//	void SuccessFadeIn(){
-//		DOTween.To (() => DratCG.alpha, x => DratCG.alpha = x, 0, TransitionTime);
-//		DOTween.To (() => SuccessCG.alpha, x => SuccessCG.alpha = x, 1, TransitionTime).OnComplete(DratFadeIn);
-//	}
+	public CanvasGroup SliderPanelCG;
+	public Slider HomeTempSlider;
+	public Text TemperatureText;
+
+	const float TransitionTime = 5f;
+
+	void Start(){
+		SliderFadeIn ();
+		HomeTempSlider.onValueChanged.AddListener (OnTempChanged);
+	}
+
+	void SliderFadeIn(){
+		DOTween.To (() => SliderPanelCG.alpha, x => SliderPanelCG.alpha = x, 1, TransitionTime);
+	}
+
+	void OnTempChanged (float f) {
+		TemperatureText.text = f.ToString();
+	}
 }
