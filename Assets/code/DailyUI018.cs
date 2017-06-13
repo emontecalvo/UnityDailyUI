5f7235251ecacb3768b8444e7d4e49a089cec24f
emontecalvo <emilymontecalvo@gmail.com>
2017-06-13 11:19:09 -0700

    Day018 - added animation to graph

--- Assets/code/DailyUI018.cs
+++ Assets/code/DailyUI018.cs
@@ -9,14 +9,57 @@ public class DailyUI018 : DailyUIBase
 	public CanvasGroup GraphCG;
 	public Button CloseButton;
 
+	public Image GraphImg;
+
 	const float TransitionTime = 1f;
 
 	void Start () {
 		CloseButton.onClick.AddListener (OnCloseClicked);
+		ChangeSize1 ();
 	}
 
 	void OnCloseClicked() {
 		DOTween.To (() => StatBoxesCG.alpha, x => StatBoxesCG.alpha = x, 0, TransitionTime);
 		DOTween.To (() => GraphCG.alpha, x => GraphCG.alpha = x, 0, TransitionTime);
 	}
+
+	void ChangeSize1 () {
+		GraphImg.rectTransform.DOScale (0.6f, 0.6f);
+		ChangeSize2 ();
+	}
+
+	void ChangeSize2 () {
+		GraphImg.rectTransform.DOScale (0.7f, 0.7f);
+		ChangeSize25 ();
+	}
+
+	void ChangeSize25 () {
+		GraphImg.rectTransform.DOScale (0.75f, 0.75f);
+		ChangeSize3 ();
+	}
+
+	void ChangeSize3 () {
+		GraphImg.rectTransform.DOScale (0.8f, 0.8f);
+		ChangeSize35 ();
+	}
+
+	void ChangeSize35 () {
+		GraphImg.rectTransform.DOScale (0.85f, 0.85f);
+		ChangeSize4 ();
+	}
+
+	void ChangeSize4 () {
+		GraphImg.rectTransform.DOScale (0.9f, 0.9f);
+		ChangeSize45 ();
+	}
+
+	void ChangeSize45 () {
+		GraphImg.rectTransform.DOScale (0.95f, 0.95f);
+		ChangeSize5 ();
+	}
+
+	void ChangeSize5 () {
+		GraphImg.rectTransform.DOScale (1f, 1f);
+	}
+	
 }
