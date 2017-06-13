e940f0e99d0f4e56f7abdfc5c7e50d78dbdc7ca4
emontecalvo <emilymontecalvo@gmail.com>
2017-06-13 10:05:24 -0700

    +Day018 - basic layout

---
+++ Assets/code/DailyUI018.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class DailyUI018 : DailyUIBase 
+{
+	public CanvasGroup StatBoxesCG;
+	public CanvasGroup GraphCG;
+	public Button CloseButton;
+
+	const float TransitionTime = 1f;
+
+	void Start () {
+		CloseButton.onClick.AddListener (OnCloseClicked);
+	}
+
+	void OnCloseClicked() {
+		DOTween.To (() => StatBoxesCG.alpha, x => StatBoxesCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => GraphCG.alpha, x => GraphCG.alpha = x, 0, TransitionTime);
+	}
+}
