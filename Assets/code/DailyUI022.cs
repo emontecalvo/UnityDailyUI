dd240af9529436798ccc8082918fd4c58391d5bf
emontecalvo <emilymontecalvo@gmail.com>
2017-06-16 10:16:31 -0700

    Day022 - search bar working

--- Assets/code/DailyUI022.cs
+++ Assets/code/DailyUI022.cs
@@ -5,61 +5,50 @@ using DG.Tweening;
 
 public class DailyUI022 : DailyUIBase 
 {
-//	public CanvasGroup StatBoxesCG;
-//	public CanvasGroup GraphCG;
-//	public Button CloseButton;
-//
-//	public Image GraphImg;
-//
-//	const float TransitionTime = 1f;
-//
-//	void Start () {
-//		CloseButton.onClick.AddListener (OnCloseClicked);
-//		ChangeSize1 ();
-//	}
-//
-//	void OnCloseClicked() {
-//		DOTween.To (() => StatBoxesCG.alpha, x => StatBoxesCG.alpha = x, 0, TransitionTime);
-//		DOTween.To (() => GraphCG.alpha, x => GraphCG.alpha = x, 0, TransitionTime);
-//	}
-//
-//	void ChangeSize1 () {
-//		GraphImg.rectTransform.DOScale (0.6f, 0.6f);
-//		ChangeSize2 ();
-//	}
-//
-//	void ChangeSize2 () {
-//		GraphImg.rectTransform.DOScale (0.7f, 0.7f);
-//		ChangeSize25 ();
-//	}
-//
-//	void ChangeSize25 () {
-//		GraphImg.rectTransform.DOScale (0.75f, 0.75f);
-//		ChangeSize3 ();
-//	}
-//
-//	void ChangeSize3 () {
-//		GraphImg.rectTransform.DOScale (0.8f, 0.8f);
-//		ChangeSize35 ();
-//	}
-//
-//	void ChangeSize35 () {
-//		GraphImg.rectTransform.DOScale (0.85f, 0.85f);
-//		ChangeSize4 ();
-//	}
-//
-//	void ChangeSize4 () {
-//		GraphImg.rectTransform.DOScale (0.9f, 0.9f);
-//		ChangeSize45 ();
-//	}
-//
-//	void ChangeSize45 () {
-//		GraphImg.rectTransform.DOScale (0.95f, 0.95f);
-//		ChangeSize5 ();
-//	}
-//
-//	void ChangeSize5 () {
-//		GraphImg.rectTransform.DOScale (1f, 1f);
-//	}
+	public CanvasGroup SearchFrameCG;
+	public CanvasGroup ResultsFrameCG;
+	public Button SearchButton;
 
+	public InputField SearchTxt;
+	public Text ResultOneTxt;
+	public Text ResultTwoTxt;
+	public Text ResultThreeTxt;
+
+	const float TransitionTime = 1f;
+	const float TransitionTime2 = 2f;
+
+	bool SearchClicked = false;
+
+	void Start () {
+		SearchButton.onClick.AddListener (OnSearchClicked);
+		DOTween.To (() => SearchFrameCG.alpha, x => SearchFrameCG.alpha = x, 1, TransitionTime).OnComplete(ResultsFadeIn);
+	}
+
+	void ResultsFadeIn() {
+		DOTween.To (() => ResultsFrameCG.alpha, x => ResultsFrameCG.alpha = x, 1, TransitionTime2);
+	}
+
+	void Update () {
+		if (SearchTxt.text == "") {
+			SearchClicked = false;
+		} else if (!SearchClicked) {
+			ResultOneTxt.text = SearchTxt.text;
+			ResultTwoTxt.text = SearchTxt.text;
+			ResultThreeTxt.text = SearchTxt.text;
+		}
+	}
+
+	void OnGUI() {
+		Event e = Event.current;
+		if (e.keyCode == KeyCode.Return) {
+			OnSearchClicked ();
+		}
+	}
+
+	void OnSearchClicked() {
+		SearchClicked = true;
+		ResultOneTxt.text = (SearchTxt.text + " people");
+		ResultTwoTxt.text = (SearchTxt.text + " world");
+		ResultThreeTxt.text = (SearchTxt.text + " there");
+	}
 }
