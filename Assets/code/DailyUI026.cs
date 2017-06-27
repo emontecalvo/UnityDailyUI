cf6add9d9617a4ce7898d4c1fa9b1cf243b0b8f8
emontecalvo <emilymontecalvo@gmail.com>
2017-06-27 09:50:30 -0700

    +Day026 - subscribe

---
+++ Assets/code/DailyUI026.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class DailyUI026 : DailyUIBase 
+{
+	public RectTransform ThankyouRT;
+	public CanvasGroup IntroCG;
+	public CanvasGroup ThankyouCG;
+	public Button SignupBtn;
+	public Button CloseBtn;
+
+	const float TransitionTime = 2f;
+
+	void Start () {
+		ThankyouRT.gameObject.SetActive (false);
+		SignupBtn.onClick.AddListener (OnSignupClicked);
+		CloseBtn.onClick.AddListener (OnCloseClicked);
+
+	}
+
+	void OnSignupClicked() {
+		ThankyouRT.gameObject.SetActive (true);
+		DOTween.To (() => IntroCG.alpha, x => IntroCG.alpha = x, 0, TransitionTime);
+		DOTween.To (() => ThankyouCG.alpha, x => ThankyouCG.alpha = x, 1, TransitionTime);
+	}
+
+	void OnCloseClicked() {
+		DOTween.To (() => ThankyouCG.alpha, x => ThankyouCG.alpha = x, 0, TransitionTime);
+	}
+}
