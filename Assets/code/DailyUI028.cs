3c80f39e8540bfd614b56a411309b6c0f5ecef12
emontecalvo <emilymontecalvo@gmail.com>
2017-07-07 17:29:36 -0700

    +Day028 - contact us

---
+++ Assets/code/DailyUI028.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class DailyUI028 : DailyUIBase 
+{
+	public CanvasGroup ContactPanelCG;
+	public Button SendButton;
+
+	const float TransitionTime = 1f;
+
+	void Start () {
+		SendButton.onClick.AddListener (OnSendClicked);
+	}
+
+	void OnSendClicked() {
+		DOTween.To (() => ContactPanelCG.alpha, x => ContactPanelCG.alpha = x, 0, TransitionTime);
+	}
+}
