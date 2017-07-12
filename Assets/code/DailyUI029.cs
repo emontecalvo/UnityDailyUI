44052627233ae1f1af29b240840c314bbfa78e34
emontecalvo <emilymontecalvo@gmail.com>
2017-07-11 17:19:13 -0700

    +Day029 - map

---
+++ Assets/code/DailyUI029.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class DailyUI029 : DailyUIBase 
+{
+	public CanvasGroup RitzPanelCG;
+	public Button BookButton;
+
+	const float TransitionTime = 1f;
+
+	void Start () {
+		DOTween.To (() => RitzPanelCG.alpha, x => RitzPanelCG.alpha = x, 1, TransitionTime);
+		BookButton.onClick.AddListener (OnBookClicked);
+	}
+
+	void OnBookClicked() {
+		DOTween.To (() => RitzPanelCG.alpha, x => RitzPanelCG.alpha = x, 0, TransitionTime);
+	}
+}
